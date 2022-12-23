e954125e04628bcb23587645735adba9139ced7e
Alex Serra Carbonell <alexsc7000@gmail.com>
2022-12-23 21:59:40 +0100

    Hola este es el meu primer commit

---
+++ src/Activitat7.java
@@ -0,0 +1,16 @@
+public class Activitat7 {
+    public static void main(String[] args) {
+
+
+          System.out.println("Hola GitHub, sóc Alex Serra");
+        System.out.println(Num20());
+}
+    public static int Num20() {
+        for (int i = 0; i < 20; i++)
+            System.out.print(i + ",");
+        {
+
+        }
+        return 20;
+    }
+}
